5f190b6776576ba9768250aed4870cbdc6a491ba
Rishi-CloudIn <rishi@cloudin.email>
2022-11-17 12:42:41 +0530

    animation added

--- src/Component/Freedom/Freedom.js
+++ src/Component/Freedom/Freedom.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Freedom.css';
 import { BsFillCheckSquareFill, BsFillArrowRightCircleFill } from 'react-icons/bs';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 
 function Freedom ()
@@ -13,37 +14,42 @@ function Freedom ()
         <div className="safe-content-div py-5">
           <div className="row">
             <div className="col-lg-5 col-12">
-              <div className="safe-img-div-1 mt-4">
-                <img
-                  src="images\lllustration-2.png"
-                  alt="img"
-                  className="safe-img img-fluid"
-                />
-              </div>
+              <AnimationOnScroll animateIn="animate__bounceInLeft">
+                <div className="safe-img-div-1 mt-4">
+                  <img
+                    src="images\lllustration-2.png"
+                    alt="img"
+                    className="safe-img img-fluid"
+                  />
+                </div>
+              </AnimationOnScroll>
             </div>
 
             <div className="col-lg-7 col-12 mt-lg-0 mt-4">
-              <ul className='ps-md-4 ps-1'>
-                <h3 className='h3 fw-bold'>LOCK - EARN - <span className='cu text-decoration-underline'>TRAVEL</span></h3>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillCheckSquareFill />
-                  </span>
-                  <span className='cu'>Lock:</span> By locking your coin, it makes it possible to give you super high APY while fulfilling your dream by making you travel to your favourite destination.
-                </li>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillCheckSquareFill />
-                  </span>
-                  <span className='cu'>Earn:</span> You will earn 2.1% per day interest, making possible to double your investment every month, and compounding $100 into $213,139 within 12 months.
-                </li>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillCheckSquareFill />
-                  </span>
-                  <span className='cu'> Travel:</span> Use our amazing NFTs to explore this beautiful world. Use your locked money each day to fulfil your desires.
-                </li>
-              </ul>
+              <AnimationOnScroll animateIn="animate__bounceInRight">
+                <ul className='ps-md-4 ps-1'>
+                  <h3 className='h3 fw-bold'>LOCK - EARN - <span className='cu text-decoration-underline'>TRAVEL</span></h3>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillCheckSquareFill />
+                    </span>
+                    <span className='cu'>Lock:</span> By locking your coin, it makes it possible to give you super high APY while fulfilling your dream by making you travel to your favourite destination.
+                  </li>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillCheckSquareFill />
+                    </span>
+                    <span className='cu'>Earn:</span> You will earn 2.1% per day interest, making possible to double your investment every month, and compounding $100 into $213,139 within 12 months.
+                  </li>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillCheckSquareFill />
+                    </span>
+                    <span className='cu'> Travel:</span> Use our amazing NFTs to explore this beautiful world. Use your locked money each day to fulfil your desires.
+                  </li>
+                </ul>
+              </AnimationOnScroll>
+
             </div>
           </div>
 
@@ -55,6 +61,7 @@ function Freedom ()
               Click Here to Buy Travelerse
             </a>
           </div>
+
         </div>
       </section>
     </>
--- src/Component/Safe/Safe.js
+++ src/Component/Safe/Safe.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Safe.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+
 import
 {
   BsFillBookmarkCheckFill,
@@ -21,52 +23,60 @@ function Safe ()
         <div className="safe-content-div">
           <div className="row">
             <div className="col-lg-5 col-12">
-              <div className="safe-img-div-1">
-                <img
-                  src="images\graph-bounce-3.png"
-                  alt="img"
-                  className="safe-img-1 img-fluid "
-                />
-              </div>
+              <AnimationOnScroll animateIn="animate__bounceInLeft">
+
+                <div className="safe-img-div-1">
+                  <img
+                    src="images\graph-bounce-3.png"
+                    alt="img"
+                    className="safe-img-1 img-fluid "
+                  />
+                </div>
+              </AnimationOnScroll>
+
             </div>
 
             <div className="col-lg-7 col-12">
-              <ul>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillBookmarkCheckFill />
-                  </span>
-                  Join Travelerse now to earn our great NFTs.
-                </li>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillBookmarkCheckFill />
-                  </span>
-                  Impossibility of extreme dump with Travelerse, because of our
-                  ACM.
-                </li>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillBookmarkCheckFill />
-                  </span>
-                  Unlike other crypto there is a 3-7% cash out limit per day of
-                  your total tokens safeguarding your hard earned money.
-                </li>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillBookmarkCheckFill />
-                  </span>
-                  The Anti-Crash Mechanism works based on the current metrics of
-                  the chart to determine the daily withdrawal limit.
-                </li>
-                <li className="h5 mt-3">
-                  <span className="cu me-3">
-                    <BsFillBookmarkCheckFill />
-                  </span>
-                  You as a small investor would achieve financial freedom,
-                  faster!
-                </li>
-              </ul>
+              <AnimationOnScroll animateIn="animate__bounceInRight">
+
+                <ul>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillBookmarkCheckFill />
+                    </span>
+                    Join Travelerse now to earn our great NFTs.
+                  </li>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillBookmarkCheckFill />
+                    </span>
+                    Impossibility of extreme dump with Travelerse, because of our
+                    ACM.
+                  </li>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillBookmarkCheckFill />
+                    </span>
+                    Unlike other crypto there is a 3-7% cash out limit per day of
+                    your total tokens safeguarding your hard earned money.
+                  </li>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillBookmarkCheckFill />
+                    </span>
+                    The Anti-Crash Mechanism works based on the current metrics of
+                    the chart to determine the daily withdrawal limit.
+                  </li>
+                  <li className="h5 mt-3">
+                    <span className="cu me-3">
+                      <BsFillBookmarkCheckFill />
+                    </span>
+                    You as a small investor would achieve financial freedom,
+                    faster!
+                  </li>
+                </ul>
+              </AnimationOnScroll>
+
             </div>
           </div>
 
